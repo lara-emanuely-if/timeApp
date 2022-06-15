1253e6086476a83abc1dacc1c33c4682a2d3c3ae
lets00 <eduardovansilva@gmail.com>
2022-06-15 14:49:31 -0300

    Fix do erro do drawer

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator} from 'react-navigation-drawer';
-
+// import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './src/pages/Home';
 import Contato from './src/pages/Contato';
 
-const Routes = createAppContainer(
-  createDrawerNavigator({
-    Contato, Home
-  }, {
-    initialRouteName: 'Home'
-  })
-);
+const Drawer = createDrawerNavigator();
 
-export default Routes;
-
- 
-  
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Contato" component={Contato} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
-
-
-export default function Contato() {
-
-    return (
-        <View style={{flex:1, alignItems:'center', justifyContent: 'center'}}>
-            <Text style={{fontSize:35}}>GGGGG</Text>
-        
-        </View>
-    );
+export default function Contato({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 35 }}>GGGGG</Text>
+    </View>
+  );
 }
